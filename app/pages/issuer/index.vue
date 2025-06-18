0d5df515489173f28dc71e8bd6e888ff6ee1f86f
Ahmed Elghareeb <me@rancho.me>
2025-06-18 17:05:31 +0200

    style: improves code styles

--- app/pages/issuer/index.vue
+++ app/pages/issuer/index.vue
@@ -7,26 +7,19 @@
         </p>
         for graduate affairs
       </div>
-      <p
-        class="mt-4 text-base leading-relaxed text-gray-600 group-hover:text-white"
-      >
+      <p class="mt-4 text-base leading-relaxed text-gray-600 group-hover:text-white">
         This platform provides different activities for ACME Schools HR
         department.
       </p>
     </div>
     <div class="grid grid-cols-1 gap-2 mt-12 sm:grid-cols-3 lg:mt-20">
-      <nuxt-link
-        v-for="page in pages"
-        :href="page.url"
-        class="transition-all duration-1000 bg-white hover:bg-blue-500 hover:shadow-xl m-2 p-4 relative z-40 group"
-      >
+      <nuxt-link v-for="page in pages" :href="page.url"
+        class="transition-all duration-1000 bg-white hover:bg-blue-500 hover:shadow-xl m-2 p-4 relative z-40 group">
         <div
-          class="absolute bg-blue-500/50 top-0 left-0 w-24 h-1 z-30 transition-all duration-200 group-hover:bg-white group-hover:w-1/2"
-        ></div>
+          class="absolute bg-blue-500/50 top-0 left-0 w-24 h-1 z-30 transition-all duration-200 group-hover:bg-white group-hover:w-1/2">
+        </div>
         <div class="py-2 px-9 relative">
-          <h3
-            class="mt-8 text-lg font-semibold text-black group-hover:text-white"
-          >
+          <h3 class="mt-8 text-lg font-semibold text-black group-hover:text-white">
             {{ page.title }}
           </h3>
           <p class="mt-4 text-base text-gray-600 group-hover:text-white">
