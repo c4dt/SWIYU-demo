60b3ffa2745fb7347087cf564866da8cc9e87e15
Ahmed Elghareeb <me@rancho.me>
2025-06-18 16:18:14 +0200

    refactor: improves styles and text within the verify page

--- app/pages/verifier/verify.vue
+++ app/pages/verifier/verify.vue
@@ -3,20 +3,12 @@
     <nav class="text-sm sm:text-base bg-white p-2 lg:p-4 rounded-md shadow-lg">
       <ol class="flex list-none p-0">
         <li class="flex items-center">
-          <NuxtLink
-            to="/verifier"
-            class="mr-2 flex items-center text-gray-600 hover:text-blue-500 transition-colors duration-300"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              height="1em"
-              viewBox="0 0 576 512"
-              class="cursor-pointer mr-1"
-              fill="currentColor"
-            >
+          <NuxtLink to="/verifier"
+            class="mr-2 flex items-center text-gray-600 hover:text-blue-500 transition-colors duration-300">
+            <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 576 512" class="cursor-pointer mr-1"
+              fill="currentColor">
               <path
-                d="M575.8 255.5c0 18-15 32.1-32 32.1h-32l.7 160.2c0 2.7-.2 5.4-.5 8.1V472c0 22.1-17.9 40-40 40H456c-1.1 0-2.2 0-3.3-.1c-1.4 .1-2.8 .1-4.2 .1H416 392c-22.1 0-40-17.9-40-40V448 384c0-17.7-14.3-32-32-32H256c-17.7 0-32 14.3-32 32v64 24c0 22.1-17.9 40-40 40H160 128.1c-1.5 0-3-.1-4.5-.2c-1.2 .1-2.4 .2-3.6 .2H104c-22.1 0-40-17.9-40-40V360c0-.9 0-1.9 .1-2.8V287.6H32c-18 0-32-14-32-32.1c0-9 3-17 10-24L266.4 8c7-7 15-8 22-8s15 2 21 7L564.8 231.5c8 7 12 15 11 24z"
-              />
+                d="M575.8 255.5c0 18-15 32.1-32 32.1h-32l.7 160.2c0 2.7-.2 5.4-.5 8.1V472c0 22.1-17.9 40-40 40H456c-1.1 0-2.2 0-3.3-.1c-1.4 .1-2.8 .1-4.2 .1H416 392c-22.1 0-40-17.9-40-40V448 384c0-17.7-14.3-32-32-32H256c-17.7 0-32 14.3-32 32v64 24c0 22.1-17.9 40-40 40H160 128.1c-1.5 0-3-.1-4.5-.2c-1.2 .1-2.4 .2-3.6 .2H104c-22.1 0-40-17.9-40-40V360c0-.9 0-1.9 .1-2.8V287.6H32c-18 0-32-14-32-32.1c0-9 3-17 10-24L266.4 8c7-7 15-8 22-8s15 2 21 7L564.8 231.5c8 7 12 15 11 24z" />
             </svg>
             Home
           </NuxtLink>
@@ -28,10 +20,7 @@
   </div>
   <div class="flex mt-14 shadow-md rounded-xl bg-gray-50 min-h-[500px]">
     <div class="flex-1 mt-10 ml-10">
-      <div
-        v-if="step === Step.VERIFICATION_FORM"
-        class="flex items-center justify-center gap-6"
-      >
+      <div v-if="step === Step.VERIFICATION_FORM" class="flex items-center justify-center gap-6">
         <form @submit.prevent="createVerificationRequest">
           <header class="pb-6 border-b border-gray-200">
             <h2 class="text-base font-semibold text-gray-900">
@@ -48,29 +37,18 @@
                 At least one must be selected.
               </p>
               <div class="flex flex-col gap-2">
-                <label
-                  v-for="option in fieldOptions"
-                  :key="option.value"
-                  class="inline-flex items-center gap-2 cursor-pointer"
-                >
-                  <input
-                    type="checkbox"
-                    class="form-checkbox accent-blue-600"
-                    :value="option.value"
-                    :checked="selectedFields.includes(option.value)"
-                    @change="() => toggleFieldChoice(option.value)"
-                  />
+                <label v-for="option in fieldOptions" :key="option.value"
+                  class="inline-flex items-center gap-2 cursor-pointer">
+                  <input type="checkbox" class="form-checkbox accent-blue-600" :value="option.value"
+                    :checked="selectedFields.includes(option.value)" @change="() => toggleFieldChoice(option.value)" />
                   <span>{{ option.label }}</span>
                 </label>
               </div>
             </div>
           </div>
           <div class="mt-6 flex items-center justify-end gap-x-6">
-            <button
-              type="submit"
-              :disabled="submitting"
-              class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-            >
+            <button type="submit" :disabled="submitting"
+              class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
               {{
                 submitting ? "Generating..." : "Generate Verification QR Code!"
               }}
@@ -78,29 +56,16 @@
           </div>
         </form>
       </div>
-      <div
-        v-if="step === Step.VERIFICATION_CREATED"
-        class="flex flex-col items-center text-center"
-      >
-        <span
-          >Please scan this QR code to accept the verifiable credential!</span
-        >
-        <qrcode-vue
-          class="mx-auto my-4"
-          :value="verificationURL"
-          level="H"
-          size="250"
-          render-as="svg"
-        />
+      <div v-if="step === Step.VERIFICATION_CREATED" class="flex flex-col items-center text-center">
+        <span>Please scan this QR code to accept the verification request!</span>
+        <qrcode-vue class="mx-auto my-4" :value="verificationURL" level="H" size="250" render-as="svg" />
         <div class="break-all">
           <div>
-            Credential ID:
+            Verification ID:
             <span class="font-mono">{{ verificationId }}</span>
           </div>
           <span>Verification URL:</span>
-          <span
-            class="font-mono text-xs bg-gray-50 rounded-md p-2 max-w-xl mx-auto break-all select-all"
-          >
+          <span class="font-mono text-xs bg-gray-50 rounded-md p-2 max-w-xl mx-auto break-all select-all">
             {{ verificationURL }}
           </span>
         </div>
@@ -113,11 +78,14 @@
           </p>
         </div>
         <KeyValueDisplay :data="disclosedData" />
+        <div class="mt-6 flex items-center justify-center gap-x-6">
+          <button type="button"
+            class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+            Generate Credential for this identity?
+          </button>
+        </div>
       </div>
-      <div
-        v-if="step === Step.VERIFICATION_FAILED"
-        class="text-red-600 text-center"
-      >
+      <div v-if="step === Step.VERIFICATION_FAILED" class="text-red-600 text-center">
         <h2 class="text-xl font-semibold mb-2">Verification Failed!</h2>
         <p class="text-sm">The holder failed to verify their credential.</p>
       </div>
@@ -126,8 +94,7 @@
       <h2 class="text-xl font-semibold mb-2">Updates</h2>
       <ol class="list-decimal ml-4 space-y-1">
         <li v-for="msg in logMessages" :key="msg.message + msg.source">
-          [{{ msg.source }}<span v-if="msg.target">&rarr; {{ msg.target }}</span
-          >] {{ msg.message }}
+          [{{ msg.source }}<span v-if="msg.target">&rarr; {{ msg.target }}</span>] {{ msg.message }}
         </li>
       </ol>
     </aside>
@@ -216,7 +183,7 @@ onMounted(() => {
         console.log("Still figuring it out...");
       }
     }
-  }, 1000);
+  }, 2500);
 });
 
 definePageMeta({ layout: "leo-inc" });
